bb8d7e1fecff5d42979b5a35cf87a9bff13a1c24
Paulo Bischof <paulo.bischof@gmail.com>
2018-11-16 14:06:56 -0200

    Fix in initial database

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Account(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
-    balance = models.DecimalField(decimal_places=3,max_digits=10)
+    balance = models.DecimalField(decimal_places=3,max_digits=25)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
