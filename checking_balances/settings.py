61ea0ecdf581b4d276e20bf8aa0b51345df45274
Paulo Bischof <paulo.bischof@gmail.com>
2018-11-17 02:32:27 -0200

    Circleci fix

--- checking_balances/settings.py
+++ checking_balances/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import uuid
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -69,11 +70,13 @@ WSGI_APPLICATION = 'checking_balances.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST_NAME': 'test_app_database_{0}'.format(uuid.uuid4()),
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
