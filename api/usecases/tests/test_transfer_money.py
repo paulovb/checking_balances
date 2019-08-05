e4b984ac7ebf669097e23f5de80d9f1f5b0b8ca0
Paulo Bischof <Paulo.Bischof@farfetch.com>
2019-08-05 21:51:17 +0100

    Test to have a transfer usecase

---
+++ api/usecases/tests/test_transfer_money.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from unittest.mock import create_autospec
+
+from api.usecases.deposit_money import DepositMoney
+from api.gateways.interfaces.financial_transaction_gateway import FinancialTransactionGateway
+from api.gateways.interfaces.account_gateway import AccountGateway
+
+from api.presenters.interfaces.financial_transaction_presenter import FinancialTransactionPresenter
+from api.structs.financial_transaction import FinancialTransaction
+from api.structs.account import Account
+
+ACCOUNT_ID_WITHDRAW = 1
+ACCOUNT_ID_DEPOSIT = 2
+VALUE = 2500.123
+NOTES = "Deposited"
+
+class TransferMoneyTests(TestCase):
+    def setUp(self):
+        self.account_gateway = create_autospec(AccountGateway)
+        self.financial_transaction_gateway = create_autospec(FinancialTransactionGateway)
+        self.financial_transaction_presenter = create_autospec(FinancialTransactionPresenter)
+
+        self.usecase = TransferMoney(
+            self.account_gateway,
+            self.financial_transaction_gateway,
+            self.financial_transaction_presenter
+        )
+
+    def test_transfer_money_by_person_one_to_person_two_by_account_id(self):
+        self.usecase.execute(ACCOUNT_ID_WITHDRAW, ACCOUNT_ID_DEPOSIT, VALUE, NOTES)
+
+        self.financial_transaction_gateway.find_transactions_by_account_id.assert_called_once_with(ACCOUNT_ID_WITHDRAW)
+        self.financial_transaction_gateway.find_transactions_by_account_id.assert_called_once_with(ACCOUNT_ID_DEPOSIT)
