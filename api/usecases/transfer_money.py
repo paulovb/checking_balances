7f204aaf549527f10b6c352223d488bfb1d52745
Paulo Bischof <Paulo.Bischof@farfetch.com>
2019-08-06 10:21:20 +0100

    Add duplicated code

--- api/usecases/transfer_money.py
+++ api/usecases/transfer_money.py
@@ -27,3 +27,13 @@ class TransferMoney(object):
 
         self.financial_transaction_presenter.show(201, account_withdraw, transactions)
 
+        
+        self.financial_transaction_gateway.registry_deposit_money(account_deposit_id, value, notes)
+        account_deposit = self.account_gateway.update_balance(account_deposit_id, float(account_deposit.balance) + float(value))
+
+        transactions = self.financial_transaction_gateway.find_transactions_by_account_id(account_withdraw_id)
+        
+        account_withdraw = self.account_gateway.get_by_id(account_withdraw_id)
+
+        self.financial_transaction_presenter.show(201, account_withdraw, transactions)
+
